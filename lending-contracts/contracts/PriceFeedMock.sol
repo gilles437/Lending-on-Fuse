cdd111cf6062a69c712a9c79a28b325328ce64ab
Gilles Barnier <gilles.barnier@gmail.com>
2024-08-19 09:18:38 +0200

    Update PriceFeedMock.sol - added usdc

--- lending-contracts/contracts/PriceFeedMock.sol
+++ lending-contracts/contracts/PriceFeedMock.sol
@@ -3,13 +3,27 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/ISupraSValueFeed.sol";
 
+
 contract PriceFeedMock is ISupraSValueFeed {
+    uint256 public constant ETH_USDT_VALUE = 2650; // ETH/USDC = 2650
+    uint256 public constant USDC_USD_VALUE = 1; // ETH/USDC = 2650
+
     function getSvalue(uint256 _pairIndex) external view override returns (priceFeed memory)
     {
         // Mocking price  values
         priceFeed memory feed;
-        // replace eth value with 2650 if needed
-        feed.price = _pairIndex == 46 ? 2650 * 1e18 : 1 * 1e18; // e.g., ETH/USDC = 2650
+        if (_pairIndex == 46) {
+            feed.price = ETH_USDT_VALUE * 1e18;
+        }
+        //USDC_USD supra oracles gives 100_000_000 for 1 USDC
+        else if (_pairIndex == 89) {
+            feed.price = USDC_USD_VALUE * 100 * 1e6;
+        } 
+        else {
+            feed.price = 1 * 1e18;
+        }
+        
+        //feed.price = _pairIndex == 46 ? ETH_USDT_VALUE * 1e18 : 1 * 1e18; // e.g., ETH/USDC = 2650
         return feed;
     }
 
@@ -18,7 +32,19 @@ contract PriceFeedMock is ISupraSValueFeed {
     {
         priceFeed[] memory feeds = new priceFeed[](_pairIndexes.length);
         for (uint256 i = 0; i < _pairIndexes.length; i++) {
-            feeds[i].price = _pairIndexes[i] == 46 ? 2650 * 1e18 : 1 * 1e18; // e.g., ETH/USDC = 2650
+
+            if (_pairIndexes[i] == 46) {
+                feeds[i].price = ETH_USDT_VALUE * 1e18;
+            }
+            //USDC_USD supra oracles gives 100_000_000 for 1 USDC
+            else if (_pairIndexes[i] == 89) {
+                feeds[i].price = USDC_USD_VALUE * 100 * 1e6;
+            } 
+            else {
+                feeds[i].price = 1 * 1e18;
+            }
+
+            //feeds[i].price = _pairIndexes[i] == 46 ? ETH_USDT_VALUE * 1e18 : 1 * 1e18; // e.g., ETH/USDC = 2650
         }
         return feeds;
     }
@@ -26,7 +52,19 @@ contract PriceFeedMock is ISupraSValueFeed {
     function getDerivedSvalue(uint256 pair_id_1, uint256 pair_id_2, uint256 operation) external view override returns (derivedData memory)
     {
         derivedData memory data;
-        data.derivedPrice = (pair_id_1 == 46 ? 2650 * 1e18 : 1 * 1e18);
+
+         if (pair_id_1 == 46) {
+                data.derivedPrice = ETH_USDT_VALUE * 1e18;
+            }
+            //USDC_USD supra oracles gives 100_000_000 for 1 USDC
+            else if (pair_id_1 == 89) {
+                data.derivedPrice = USDC_USD_VALUE * 100 * 1e6;
+            } 
+            else {
+                data.derivedPrice = 1 * 1e18;
+            }
+
+        //data.derivedPrice = (pair_id_1 == 46 ? ETH_USDT_VALUE * 1e18 : 1 * 1e18);
         return data;
     }
 
